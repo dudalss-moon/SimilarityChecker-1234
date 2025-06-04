65f5cca07157e0484246de39801a38105b709c0f
dudalss <myemail@emap.com>
2025-06-04 16:52:06 +0900

    length commit

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,24 @@
 public class SimilarityChecker {
+
+    public static final int MAX_SCORE = 60;
+
+    public int getLengthScore(String fistString, String secondString) {
+        if (isSameLength(fistString, secondString)) {
+            return MAX_SCORE;
+        }
+        return getLengthScoreForPartialMatched(fistString, secondString);
+    }
+
+    private boolean isSameLength(String fistString, String secondString) {
+        return fistString.length() == secondString.length();
+    }
+
+    private int getLengthScoreForPartialMatched(String fistString, String secondString) {
+        int gapCount = Math.abs(fistString.length() - secondString.length());
+        int minCount = Math.min(fistString.length(), secondString.length());
+
+        double rate = 1 - (gapCount / (double) minCount);
+        int score = (int) (rate * MAX_SCORE);
+        return score;
+    }
 }
