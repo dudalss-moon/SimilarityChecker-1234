76eec517763f2354a564de6dfde698f6bd826786
unknown <test@test.com>
2025-06-08 15:58:03 +0900

    mod

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -7,14 +7,14 @@ public class SimilarityChecker {
             return MAX_SCORE;
         }
 
-        if (isTowMultipleLength(fistString, secondString)) {
+        if (isDoubleleLength(fistString, secondString)) {
             return 0;
         }
 
         return getLengthScoreForPartialMatched(fistString, secondString);
     }
 
-    private boolean isTowMultipleLength(String fistString, String secondString) {
+    private boolean isDoubleleLength(String fistString, String secondString) {
         int gapCount = Math.abs(fistString.length() - secondString.length());
         int minCount = Math.min(fistString.length(), secondString.length());
 
@@ -35,5 +35,79 @@ public class SimilarityChecker {
         double rate = 1 - (gapCount / (double) minCount);
         int score = (int) (rate * MAX_SCORE);
         return score;
+    }
+
+    public int getAlphabetScore(String firstAlphabet, String secondAlphabet) throws RuntimeException {
+        checkValidArgument(firstAlphabet);
+        checkValidArgument(secondAlphabet);
+
+        int[] array = new int[24];
+        for (char ch : firstAlphabet.toCharArray()) {
+            // only first
+            array[ch - 65] = 1;
+        }
+        for (char ch : secondAlphabet.toCharArray()) {
+            if (array[ch - 65] == 0) {
+                // only second
+                array[ch - 65] = 2;
+            } else if (array[ch - 65] == 1) {
+                // both
+                array[ch - 65] = 3;
+            }
+        }
+
+        int firstCount = 0;
+        int secondCount = 0;
+        int bothCount = 0;
+        for (int i = 0; i < 24; i++) {
+            if (array[i] == 1) {
+                firstCount++;
+            } else if (array[i] == 2) {
+                secondCount++;
+            } else if (array[i] == 3) {
+                bothCount++;
+            }
+        }
+        if (bothCount > 0 && firstCount == 0 && secondCount == 0) {
+            return 40;
+        } else if (bothCount == 0) {
+            return 0;
+        } else {
+            double score = (double) bothCount / (firstCount + secondCount + bothCount) * 40;
+            return (int) score;
+        }
+
+//        int firstAlphabetCount = getAlphabetCount(getBoolCheckedAlphabet(firstAlphabet));
+//        int secondAlphabetCount = getAlphabetCount(getBoolCheckedAlphabet(secondAlphabet));
+//        if (firstAlphabetCount == secondAlphabetCount) {
+//            return 40;
+//        }
+//        return 0;
+    }
+
+    private int getAlphabetCount(boolean[] alphabetArray) {
+        int alphabetCount = 0;
+        for (int i = 0; i < 24; i++) {
+            if (alphabetArray[i]) {
+                alphabetCount++;
+            }
+        }
+        return alphabetCount;
+    }
+
+    private boolean[] getBoolCheckedAlphabet(String alphabetString) {
+        boolean[] checkedArray = new boolean[24];
+        for (char ch : alphabetString.toCharArray()) {
+            checkedArray[ch - 65] = true;
+        }
+        return checkedArray;
+    }
+
+    private void checkValidArgument(String str) {
+        for (char ch : str.toCharArray()) {
+            if (ch < 'A' || ch > 'Z') {
+                throw new RuntimeException();
+            }
+        }
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -41,5 +42,30 @@ class SimilarityCheckerTest {
     @DisplayName("부분 점수 GAP(긴문자열 - 짧은 문자열)에 따라 점수 - AA, AAE")
     void getPartialScoreAnother() {
         assertEquals(30, similarityChecker.getLengthScore("AA", "AAE"));
+    }
+
+    @Test
+    @DisplayName("같은 종류의 알파벳 전체 사용 검사 - ASD, DSA")
+    void getSameUseAllKindOfAlphabet() {
+        assertEquals(40, similarityChecker.getAlphabetScore("ASD", "DSA"));
+    }
+
+    @Test
+    @DisplayName("사용된 알파벳 종류가 전부 다른 검사 - A, BB")
+    void getDiffUseAllKindOfAlphabet() {
+        assertEquals(0, similarityChecker.getAlphabetScore("A", "BB"));
+    }
+
+    @Test
+    @DisplayName("부분 점수 계산 검증 - AA, AAE")
+    void getPartialSameUseAllKindOfAlphabet() {
+        assertEquals(20, similarityChecker.getAlphabetScore("AA", "AAE"));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("대문자만 취급")
+    void checkOnlyAlphabet() {
+        assertNotEquals(RuntimeException.class, similarityChecker.getAlphabetScore("1", "323"));
     }
 }
